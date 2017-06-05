ca2876dc62187bd94a13ea97eb96d9c5004e316b
Todd G. Hetrick <toddghetrick@gmail.com>
2017-06-05 11:10:52 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Todd G. Hetrick
+ * Last Updated: 6/6/2017
+ *
+ * Required Env Vars:
+ *    AWS_REGION
+ *    QUEUE_URL
+ */
+'use strict';
+
+var AWS = require('aws-sdk');
+var sqs = new AWS.SQS({region : process.env.AWS_REGION});
+
+exports.handler = function(event, context) {
+  var params = {
+    MessageBody: JSON.stringify(event),
+    QueueUrl: process.env.QUEUE_URL
+  };
+  sqs.sendMessage(params, function(err,data){
+    if(err) {
+      console.log('error:',"Fail Send Message" + err);
+      context.done('error', "ERROR Put SQS");  // ERROR with message
+    } else {
+      console.log('data:',data.MessageId);
+      context.done(null,'');  // SUCCESS 
+    }
+  });
+}
